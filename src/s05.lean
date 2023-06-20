fb4b799d4cdd95ffb887c8c2f007aafa08a55a81
Luca Leon Happel <luca.happel@hhu.de>
2023-06-20 15:35:20 +0200

    source klappt, target noch nicht so ganz

--- src/s05.lean
+++ src/s05.lean
@@ -1,5 +1,6 @@
 import topology.basic
 import topology.path_connected
+import topology.metric_space.basic
 import analysis.inner_product_space.basic
 import analysis.normed_space.basic
 import data.set.basic
@@ -23,7 +24,20 @@ und $H(x,1) = 0$ für alle $x \in S^1$. Somit ist $f$ nullhomotop
 und $S^2$ einfach-zusammenhängend.
 -/
 
+-- definiere ℝ×ℝ×ℝ als topologischen Raum
 example : topological_space (ℝ×ℝ×ℝ) := by apply_instance 
+
+-- definiere ℝ×ℝ×ℝ als metrischen Raum
+instance : metric_space (ℝ×ℝ×ℝ) := by apply_instance
+
+-- Zeige, dass die Norm ℝ×ℝ×ℝ → S2 stetig ist
+-- lemma norm_continuous_on_S2 : continuous_on (norm : (ℝ × ℝ × ℝ) → ℝ) S2 :=
+-- begin
+--   rw continuous_on_iff_continuous_restrict,
+--   apply continuous_norm.comp,
+--   --apply continuous_subtype_val.continuous_on,
+--   sorry
+-- end
 
 example : @ is_simply_connected _ _ (1,0,0) S2 _ :=
 begin
@@ -44,25 +58,51 @@ begin
       intro y,
       intro hy,
       -- decompose y into y1, y2, y3
-      cases y with y1 y23,
-      cases y23 with y2 y3,
-      let y := (y1,y2,y3),
+      -- cases y with y1 y23,
+      -- cases y23 with y2 y3,
+      -- let y := (y1,y2,y3),
       unfold joined_in,
       -- Wir möchten nun den Pfad von (1,0,0) nach y konstruieren.
       let f : path (1,0,0) y := {
         -- betrachte die Definition von `path` mit <kbd>ctrl</kbd>+<kbd>click</kbd> auf `path`
         -- wir brauchen also auf jeden fall:
-        source' := by simp,
-        target' := by simp,
+        source' := begin
+          -- Hier möchten wir zeigen, dass (1,0,0) die Quelle von f ist.
+          simp,
+          unfold norm,
+          simp,
+        end,
+        target' := begin
+          simp,
+          -- Hier möchten wir zeigen, dass wir auf jeden Fall am Ende unseres Pfades bei y ankommen.
+          -- Wir haben zwei fälle: y = (1,0,0) und y ≠ (1,0,0)
+          -- Wir müssen einen extra Fall für y = (1,0,0) machen, unser Pfad in diesem fall besonders definiert werden muss,
+          --  damit keine Singularität auftritt.
+          cases eq_or_ne y (1, 0, 0) with h h,
+          {
+            -- wenn y = (1,0,0)
+            rw h,
+            unfold norm,
+            simp,
+          },
+          {
+            -- wenn y ≠ (1,0,0)
+            rw if_neg h,
+            sorry,
+          },
+        end,
         -- target' := _,
         -- außerdem steht dort `extends C(I, X)`, also brauchen wir noch:
         -- hier soll eine funktion definiert werden, welche [0,1] nach ℝ×ℝ×ℝ geht
         -- sie soll für t = 0 den wert (1,0,0) haben und für t = 1 den wert y
         -- wir müssen auf jeden Fall für jeden wert t zwischen 0 und 1 einen wert p in ℝ×ℝ×ℝ haben, mit ‖p‖ = 1
         to_fun := λ t,
-          let v := (1 - (coe t : ℝ)) • (1,0,0) + (coe t : ℝ) • y in
-          (1 / ‖v‖) • v,
-        continuous_to_fun := by continuity,
+          if y = (1,0,0) then
+            (1,0,0)
+          else
+            let v := (1 - (coe t : ℝ)) • (1,0,0) + (coe t : ℝ) • y in
+            (1 / ‖v‖) • v,
+        continuous_to_fun := sorry,
       },
       use f,
       intro y',
